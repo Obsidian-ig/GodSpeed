baf943e0952759afbd9f309f720481980f5897ef
Obsidian-ig <villocityontiktok@gmail.com>
2023-12-27 13:55:47 -0800

    retry upload

---
+++ Patches/PlayerPatch.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using Dissonance;
+using GameNetcodeStuff;
+using HarmonyLib;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Windows;
+
+namespace LethalCompanyMods.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+
+    internal class PlayerControllerBPatch
+    {
+
+        [HarmonyPatch(typeof(PlayerControllerB))]
+        internal class PlayerControllerPatch
+        {
+            [HarmonyPatch("Update")]
+            [HarmonyPostfix]
+            private static void SpeedPatch(ref float ___sprintMultiplier)
+            {
+                ___sprintMultiplier = 4f;
+            }
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+
+# Obsidians Fast Climb Mod
+
+This mod allows you to scale/climb in lethal company very fast.
+
+
+
+
+## Acknowledgements
+
+ - This mod is complete and will probably not be updated
+ - I created it in about 5 minutes
+ - I will not update it anymore
+
+
+## Authors
+
+- [@Obsidian-ig](https://www.github.com/obsidian-ig)
+
+
+## 🔗 Links
+[![github-download](https://res.cloudinary.com/practicaldev/image/fetch/s--WDdeolgp--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/tk4tr033eq3ggxit0xti.png)](https://github.com/Obsidian-ig/FastClimb)
+
+
